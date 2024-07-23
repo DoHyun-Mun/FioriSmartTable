5e95fb7ed19dad254e3621838afcf88e41a2efe6
dohyun.mun <71109578+DoHyun-Mun@users.noreply.github.com>
2024-07-23 19:17:07 +0900

    Update Detail.controller.js
    update added

--- webapp/controller/Detail.controller.js
+++ webapp/controller/Detail.controller.js
@@ -16,7 +16,73 @@ sap.ui.define([
             _onObjectMatched: function(oEvent) {
                 this.pQcode = "/" + oEvent.getParameter("arguments").qcode;
                 this.getView().bindElement({path: this.pQcode});
-            }
+            },
 
+            onBack: function () {
+                this.getOwnerComponent().getRouter().navTo("RouteMain");
+            },
+
+            onUpdate: async function () {
+
+                // await setBusy(true);
+    
+                let url = "/v2/dsodata/DS_01(qcode='"+this.getView().byId("i_qcode").getValue()+"')";
+                let detailData = {};
+                detailData.cost1 = this.getView().byId("i_cost1").getValue();
+                detailData.curr1 = this.getView().byId("i_curr1").getValue();
+                detailData.cost2 = this.getView().byId("i_cost2").getValue();
+                detailData.curr2 = this.getView().byId("i_curr2").getValue();
+                detailData.date1 = this.getView().byId("i_date1").getValue();
+                
+                await this.patch(url, detailData);
+                
+                this.getView().getModel().refresh();             
+                this.getOwnerComponent().getRouter().navTo('RouteMain');
+
+            },
+
+            patch: function (url, data) {
+                return new Promise((resolve) => {
+                    $.ajax({
+                        url: url,
+                        type: "GET",
+                        beforeSend: function (xhr) {
+                            BusyIndicator.show();
+                            xhr.setRequestHeader('X-CSRF-Token', "Fetch");
+                        }
+                    })
+                    .done((result, textStatus, xhr) => {
+                        let token = xhr.getResponseHeader("X-CSRF-Token");
+                        $.ajax({
+                            type: 'PATCH',
+                            async: false,
+                            data: JSON.stringify(data),
+                            url: url,
+                            beforeSend: function (xhr) {
+                                xhr.setRequestHeader('X-CSRF-Token', token);
+                                xhr.setRequestHeader('Content-type', 'application/json');
+                            },
+                            complete: function() {
+                                BusyIndicator.hide();
+                            }
+                        })
+                            .done(function (status) {
+                                resolve(status);
+
+                            })
+                            .fail(function (xhr) {
+                                resolve(xhr);
+                                MessageBox.error("Patch Request Failed")
+                                console.log(xhr);
+                            })
+                    })
+                    .fail(function (xhr) {
+                        MessageBox.error("Token Request Failed");
+                        console.log(xhr);
+                    }                    
+                )
+                    
+                });
+            }
         });
     });
